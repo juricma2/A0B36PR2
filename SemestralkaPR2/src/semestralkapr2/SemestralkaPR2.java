f97cba135dcdf056bc9b7f1561bb6ead58510ecf
Administrator <Administrator@147.32.219.148>
2013-02-21 16:14:35 +0100

    Prvni commit - vytvoreni projektu

---
+++ SemestralkaPR2/src/semestralkapr2/SemestralkaPR2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semestralkapr2;
+
+/**
+ *
+ * @author Administrator
+ */
+public class SemestralkaPR2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
